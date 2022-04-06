f6f4375412633e7eb01c3cd6355df58366488188
Angel <angelcruz981@yahoo.com>
2022-04-05 20:02:20 -0700

    chore(FB): FB component might need this I'm honestly not sure

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import "../styles/globals.css";
+import Navbar from "../components/navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const facebookScript = document.createElement("script");
+    facebookScript.async = true;
+    facebookScript.src =
+      "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v13.0";
+    document.body.appendChild(facebookScript);
+  }, []);
+
+  return (
+    <>
+    <Navbar/>
+  <Component {...pageProps} />
+  </>
+  
+  )
 }
 
-export default MyApp
+export default MyApp;

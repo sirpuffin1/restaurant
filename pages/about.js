672d866e8bf94f8d61afc5f77ea6498459ffe35f
Angel <angelcruz981@yahoo.com>
2022-04-20 16:36:14 -0700

    feat(about): create and configure about page

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,36 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../public/logo.png';
+import styles from '../styles/About.module.css';
+import Carousel from '../components/Carousel';
+import { slideByIndex } from '../public/aboutGallery';
+
+const SLIDE_COUNT = 6;
+const slides = Array.from(Array(SLIDE_COUNT).keys());
+const props = {
+    slides: slides,
+    images: slideByIndex
+  }
 
 const About = () => {
     return (
-        <div>
-            about is working!
-            <Link href="/">
-                <a>Home</a>
-            </Link>
+        <>
+        <div className={styles.introContainer}>
+        <h1>Rock and Wings Experience</h1>
+        <div className={styles.imageContainer}>
+             
+        <Image src={logo} layout="fill" className={styles.image}/>
         </div>
+        <p className={styles.aboutText} >
+        Come and ROCK ON watch your favorite games & music videos. Get the feel of a ROCK STAR.
+We have a selection of burgers, wings, salads, wraps and beer.
+ 
+        </p>
+        </div>
+
+<Carousel {...props}/>
+
+        </>
     );
 }
 
